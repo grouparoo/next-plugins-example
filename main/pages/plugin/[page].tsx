7684b2ad83c524c3c8b81a881579e89e7b5ee15c
Evan Tahler <evantahler@gmail.com>
2020-05-11 08:58:35 -0700

    use lerna to install everything

--- main/pages/plugin/[page].tsx
+++ main/pages/plugin/[page].tsx
@@ -9,9 +9,9 @@ export default function PluginContainerPage() {
   }
 
   const PluginComponent = dynamic(
-    () => import(`./../../node_modules/plugin/pages/${router.query.page}`),
+    () => import(`./../../../plugin/pages/${router.query.page}`),
     {
-      loading: () => <p>Loading...</p>
+      loading: () => <p>Loading...</p>,
     }
   );
 
